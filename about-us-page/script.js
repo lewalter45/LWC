4959f5ffe861b6b19086d16449f5ff5cb7966ae8
William Lynch <139822339+lewalter45@users.noreply.github.com>
2024-02-22 23:08:15 -0600

    Adjusted the Drop-Down Menu.

--- about-us-page/script.js
+++ about-us-page/script.js
@@ -1,2 +1,11 @@
 const checkbox = document.getElementById("menu-toggle");
 checkbox.addEventListener("change", toggleMenu);
+
+function toggleMenu() {
+  const menuItems = document.querySelector(".menu-items");
+  if (this.checked) {
+    menuItems.style.transform = "translateX(0)";
+  } else {
+    menuItems.style.transform = "translateX(-150%)";
+  }
+}
